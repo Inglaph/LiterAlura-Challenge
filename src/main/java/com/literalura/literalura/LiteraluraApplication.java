f4ee6221b3f0bdef0a3cd0af4adbc3ae1f2a2528
inglap-w10 <inglaph@gmail.com>
2024-07-06 19:26:37 +0200

    Solucionado guardar un mismo autor repetido en autores  si tiene varios libros

--- src/main/java/com/literalura/literalura/LiteraluraApplication.java
+++ src/main/java/com/literalura/literalura/LiteraluraApplication.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Scanner;
 
@@ -16,12 +17,18 @@ import java.util.Scanner;
 public class LiteraluraApplication implements CommandLineRunner {
 	@Autowired
 	private LibroRepository libroRepository;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		// Ajusta este comando SQL según tu base de datos
+		String sql = "ALTER SEQUENCE autores_id_seq RESTART WITH 18;";
+		jdbcTemplate.execute(sql);
 		Principal principal = new Principal(libroRepository);
 		principal.mostrarMenu();
 
